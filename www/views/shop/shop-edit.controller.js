a8265dc7ec8e6f32a84a07ff6bd9ba73e2a8a52d
Caixianluaaaa <32267787+Caixianluaaaa@users.noreply.github.com>
2017-10-19 19:20:30 +0800

    cxl

---
+++ www/views/shop/shop-edit.controller.js
@@ -0,0 +1,21 @@
+﻿(function () {
+  'use strict';
+  angular.module('starter.controllers').controller('ShopEditCtrl',['$scope','$stateParams','localStorageService',function ($scope,$stateParams,localStorageService) {
+    $scope.title = $stateParams.title;
+    $scope.property = $stateParams.property;
+    $scope.shop = localStorageService.get('Shop',{
+      name:'蔡仙禄',
+      ab:'',
+      name1:'蔡仙禄',
+      phone:'15659793538',
+      phone1:'15659793538',
+      createTime:'2017-9-28 15:30:00',
+      email:'1251975954@qq.com',
+      hylx:'111'
+
+    });
+    $scope.save = function () {
+      localStorageService.update('Shop',$scope.shop);
+    };
+  }]);
+})();
---
+++ www/views/shop/shop.controller.js
@@ -0,0 +1,26 @@
+﻿(function () {
+  'use strict';
+  angular.module('starter.controllers').controller('ShopCtrl',['$scope','localStorageService','$state',function ($scope,localStorageService,$state) {
+    $scope.shop={};
+
+    $scope.editShop=function (titleValue,propertyValue) {
+      $state.go('app.shop-edit',{
+        title:titleValue,
+        property:propertyValue
+      });
+    };
+
+    $scope.$on('$stateChangeSuccess',function () {
+      $scope.shop=localStorageService.get('Shop',{
+        name:'蔡仙禄',
+        ab:'',
+        name1:'刘智勇',
+        phone:'15659793538',
+        phone1:'15659793538',
+        createTime:'2017-9-28 15:30:00',
+        email:'1251975954@qq.com',
+        hylx:'111'
+      });
+    });
+  }])
+})();
